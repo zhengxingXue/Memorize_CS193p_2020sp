3a2ccf6904737d15aa16fe455e409748c732ca0f
zhengxingXue <zx252cornell@gmail.com>
2021-05-03 15:43:16 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jim's MacBook Pro on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
